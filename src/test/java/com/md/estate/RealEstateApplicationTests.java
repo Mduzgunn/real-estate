13558f6eb0352a63019a54665687d20f3a8df422
Melih Düzgün <aduzgun99@gmail.com>
2023-04-29 22:42:32 +0300

    models created

---
+++ src/test/java/com/md/estate/RealEstateApplicationTests.java
@@ -0,0 +1,13 @@
+package com.md.estate;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class RealEstateApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
